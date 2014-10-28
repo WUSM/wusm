a7f738f663222e440e8355749ef773a51f87392f
Aaron Graham <aegraham@gmail.com>
2014-10-28 13:45:28 -0500

    Moving out of jPanel New mobile menu v 1.0

--- _/js/functions.js
+++ _/js/functions.js
@@ -15,7 +15,7 @@
 /* trigger when page is ready */
 $(document).ready(function (){
     // jPanelMenu
-    var jPM = $.jPanelMenu({
+    /*var jPM = $.jPanelMenu({
         menu                : '#mobile-nav',
         trigger             : '#mobile-menu-icon',
         direction           : 'right',
@@ -37,11 +37,31 @@ $(document).ready(function (){
         $(this).toggleClass("dashicons-arrow-up-alt2 dashicons-arrow-down-alt2");
     });
 
-    $("#jPanelMenu-menu .current_page_ancestor > .children").addClass("jpm-expanded").slideToggle();
+    $("#jPanelMenu-menu .current_page_ancestor > .children").addClass("jpm-expanded").slideToggle();*/
+
     // Remove inline style (hard-coded width)
     // Remove extra 10px of width from inline style
     //$(".wp-caption").removeAttr('style');
+    $('#mobile-menu-icon').click(function() {
+        $('#mobile-nav').slideToggle();
+    });
     
+    $('#mobile-search-icon').click(function() {
+        $('#mobile-search-form').slideToggle();
+    });
+
+    $("#mobile-nav .page_item_has_children > a").each(function() {
+        $(this).after( "<div class='dashicons dashicons-arrow-down-alt2 jpm-expand'></div>" );
+    });
+
+    $(".jpm-expand").click( function() {
+        if( $(this).parent().parent().attr('id') == 'jPanelMenu-menu' ){
+            $(".jpm-expanded").not($(this).next()).removeClass("jpm-expanded").slideUp();
+            $(".jpm-expand").not($(this)).addClass("dashicons-arrow-down-alt2").removeClass("dashicons-arrow-up-alt2");
+        }
+        $(this).next().toggleClass("jpm-expanded").slideToggle();
+        $(this).toggleClass("dashicons-arrow-up-alt2 dashicons-arrow-down-alt2");
+    });
 });
 
 
