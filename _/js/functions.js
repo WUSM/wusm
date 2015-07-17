d2ee4d1adbd7ab9d4ed3493a2f9788cc96b0e10a
Sam Hermes <sam.hermes@wustl.edu>
2015-07-16 21:19:34 -0500

    Convert mobile menu to design used on School of Medicine

--- _/js/functions.js
+++ _/js/functions.js
@@ -5,32 +5,33 @@
 /* trigger when page is ready */
 $(document).ready(function (){
     $('#mobile-menu-icon').click(function() {
-        $('#mobile-nav').slideToggle();
-        $(".dashicons-arrow-down-alt2").each(function() {
-            if($(this).parent().height() != 0)
-                $(this).height($(this).parent().height() - 26);
-        });
+        $('#mobile-nav').slideToggle('fast');
     });
     
     $('#mobile-search-icon').click(function() {
-        $('#mobile-search-form').slideToggle();
+        $('#mobile-search-form').slideToggle('fast');
+        $('#mobile-search-form').toggleClass('active');
     });
 
-    $("#mobile-nav .page_item_has_children > a").each(function() {
-        $(this).after( "<div class='dashicons dashicons-arrow-down-alt2 jpm-expand'></div>" );
+    $('#mobile-nav').find('.page_item_has_children > a').each(function() {
+        $(this).wrap( '<div></div>' );
     });
 
-    $(".jpm-expand").click( function() {
-        if( $(this).parent().parent().parent().attr('id') == 'mobile-nav' ){
-            $(".jpm-expanded").not($(this).next()).removeClass("jpm-expanded").slideUp();
-            $(".jpm-expand").not($(this)).addClass("dashicons-arrow-down-alt2").removeClass("dashicons-arrow-up-alt2");
+    $("#mobile-nav .page_item_has_children > div > a").each(function() {
+        $(this).after( "<div class='dashicons dashicons-arrow-down-alt2 expand'></div>" );
+    });
+
+    $(".expand").click( function() {
+        if( $(this).parent().parent().parent().parent().attr('id') == 'mobile-nav' ){
+            $(".expanded").not($(this).parent().next()).removeClass("expanded").slideUp();
+            $(".expand").not($(this)).addClass("dashicons-arrow-down-alt2").removeClass("dashicons-arrow-up-alt2");
         }
-        $(this).next().toggleClass("jpm-expanded").slideToggle();
+        $(this).parent().next().toggleClass("expanded").slideToggle('fast');
         $(this).toggleClass("dashicons-arrow-up-alt2 dashicons-arrow-down-alt2");
     });
 
-    $("#mobile-nav .current_page_ancestor > .children").addClass("jpm-expanded").slideToggle();
-    $("#mobile-nav .current_page_ancestor > .dashicons-arrow-down-alt2").toggleClass("dashicons-arrow-up-alt2 dashicons-arrow-down-alt2");
+    $("#mobile-nav .current_page_ancestor > .children").addClass("expanded").slideToggle();
+    $("#mobile-nav .current_page_ancestor > div .dashicons-arrow-down-alt2").toggleClass("dashicons-arrow-up-alt2 dashicons-arrow-down-alt2");
 
     if($('#lists > div').length == 1) {
         $('#col1').css('width', 'auto');
