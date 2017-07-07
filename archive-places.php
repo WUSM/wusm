b3b58e74e715378678d109b53bd7abeff7a6e0ae
Aaron Graham <aaron.graham@wustl.edu>
2017-07-07 15:20:29 -0500

    Fix for Archive titles

--- archive-places.php
+++ archive-places.php
@@ -5,9 +5,10 @@
 	<div class="wrapper clearfix">
 		<div id="page-background-inner"></div>
 		<?php get_sidebar( 'left' );
-		if ( have_posts() ) :
-			echo '<article>';
-			the_title( '<h1>', '</h1>' );
+		if ( have_posts() ) : ?>
+			<article>
+			<h1>Places</h1>
+			<?php
 			if ( function_exists( 'washu_ppi_loop_controller' ) ) {
 				washu_ppi_loop_controller( get_post_type(), true, 'list' ); // Loop and retrieve appropriate template partial(s)
 				add_action( 'wp_footer', function() {
