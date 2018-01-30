dce708416bd10387fb8a9f2280929066994a114f
Brian Marston <brianmarston@gmail.com>
2018-01-30 16:12:23 -0600

    Internationalization (i18n) for dian.wustl.edu

--- searchform.php
+++ searchform.php
@@ -1,5 +1,5 @@
 <form name="search" method="get" id="search-form" autocapitalize="none" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-    <p><label for="search-box">Search</label><br>
+    <p><label for="search-box"><?php _e( 'Search', 'wusm' ); ?></label><br>
     <input type="text" id="search-box" name="s">
     <button type="submit" class="submit" id="search-btn"><img src="<?php echo get_template_directory_uri(); ?>/_/img/search.svg" onerror="this.src='<?php echo get_template_directory_uri(); ?>/_/img/search.png';this.onerror=null;"></button>
     </p>
